94cdde5746b9e5fab06bf57b20d634e57119f484
Vaibhav <Vaibhav@192.168.1.27>
2024-02-07 14:37:34 +0530

    Classb

--- src/test/java/practice/SimpleDemo12.java
+++ src/test/java/practice/SimpleDemo12.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SimpleDemo1
+public class SimpleDemo12
 {
 	public static void main(String[] args) throws Throwable {
 	WebDriver	driver=new FirefoxDriver();
